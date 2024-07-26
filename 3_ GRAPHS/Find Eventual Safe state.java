14eb2c638a1c5d1537b825475ab3587a6712f68b
vaibhav Soni <vaibhavsoni1712@gmail.com>
2024-07-26 19:54:40 +0530

    commit

---
+++ 3_ GRAPHS/Find Eventual Safe state.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj){
+         List<List<Integer>> adjRev = new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adjRev.add(new ArrayList<>());
+        }
+        int[] indegree = new int[V];
+        for(int i=0;i<V;i++){
+            for(int it:adj.get(i)){
+                adjRev.get(it).add(i);
+                indegree[i]++;
+            }
+        }
+        Queue<Integer>q = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty()){
+            int node = q.poll();
+            ans.add(node);
+            for(int it:adjRev.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.add(it);
+                }
+            }
+        }
+        Collections.sort(ans);
+        return ans;
+    }
+}
