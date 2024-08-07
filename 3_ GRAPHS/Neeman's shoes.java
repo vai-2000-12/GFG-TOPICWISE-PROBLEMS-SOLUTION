b9712f07e18ab0453e68e9fad51db44635c76ab3
vaibhav Soni <vaibhavsoni1712@gmail.com>
2024-08-07 14:22:17 +0530

    commit

--- 3_ GRAPHS/Neeman's shoes.java
+++ 3_ GRAPHS/Neeman's shoes.java
@@ -1,69 +1,86 @@
-// Tomorrows Submission:-
 import java.util.*;
 
-class Pair implements Comparable<Pair>{
-    int x;
-    int steps;
+class Pair implements Comparable<Pair>
+{
+    int dist, node;
+    Pair(int n, int d)
+    {
+        dist = d;
+        node = n;
+    }
     
-    public Pair(int x ,int steps){
-        this.x = x;
-        this.steps = steps;
+    public int compareTo(Pair p)
+    {
+        return dist-p.dist;
     }
-    public int compareTo(Pair p){
-        return this.steps - p.steps;
-    }
+    
 }
+
 class Solution
 {
     public String exercise(int N, int M, ArrayList<ArrayList<Integer>> A, int src, int dest, int X)
     {
-        ArrayList<ArrayList<Pair>> graph = new ArrayList<>();
-        for( int i =0 ; i < N ; i++){
-            graph.add(new ArrayList<>());
-        }
-        
-        for( int i = 0 ; i < M ; i++){
-         
-            int u = A.get(i).get(0);
-            int v = A.get(i).get(1);
-            int w = A.get(i).get(2);
-            
-          graph.get(u).add(new Pair(v, w));
-        }
-        
-        boolean[]visited = new boolean[N];
-        Arrays.fill(visited , false);
-        
         PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.add(new Pair(src, 0));
-
         
-        while(!pq.isEmpty()){
-            Pair p = pq.poll();
-            int node = p.x;
-            int st = p.steps;
-            
-            if(node == dest){
-                return st <= X ? "Neeman's Cotton Classics" : "Neeman's Wool Joggers";
+        ArrayList<Pair> adj[] = new ArrayList[N];
+        
+        for(int i=0; i<M; i++)
+        {
+            int n1 = A.get(i).get(0);
+            int n2 = A.get(i).get(1);
+            int d = A.get(i).get(2);
+            //System.out.println(n1 + " " + n2 + " " + d);
+            if(adj[n1]==null)
+            {
+                adj[n1] = new ArrayList<>();
             }
             
-             if (!visited[node]) {
-                visited[node] = true;
-                
-                for (Pair neighbor : graph.get(node)) {
-                    if (!visited[neighbor.x]) {
-                        pq.add(new Pair(neighbor.x, st + neighbor.steps));
-                    }
+            if(adj[n2]==null)
+            {
+                adj[n2] = new ArrayList<>();
+            }
+            
+            adj[n1].add(new Pair(n2, d));
+            adj[n2].add(new Pair(n1, d));
+            
+        }
+        
+        pq.add(new Pair(src, 0));
+        
+        boolean visited[] = new boolean[N];
+        
+        while(pq.isEmpty()==false)
+        {
+            
+            Pair p = pq.poll();
+            
+            visited[p.node] = true;
+            
+            if(p.node==dest)
+            {
+                if(p.dist<=X)
+                {
+                    return "Neeman's Cotton Classics";
+                }
+                else
+                {
+                    return "Neeman's Wool Joggers";
                 }
             }
-          }
+            
+            for(Pair nei : adj[p.node])
+            {
+                if(visited[nei.node]==false)
+                {
+                    pq.add(new Pair(nei.node, nei.dist + p.dist));
+                }
+            }
+            
+            
+        }
+        
         
         return " ";
+        
     }
 }
-
-/**
- * The `Solution` class contains a method `exercise` that uses Dijkstra's algorithm to find the
- * shortest path in a graph and returns a specific string based on the path length compared to a given
- * threshold.
- */
