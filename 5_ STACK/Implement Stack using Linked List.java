424158153bfba319d7add2303d7a71cb1aa22b54
vaibhav Soni <vaibhavsoni1712@gmail.com>
2024-08-09 23:19:25 +0530

    commit

---
+++ 5_ STACK/Implement Stack using Linked List.java
@@ -0,0 +1,32 @@
+class MyStack {
+    // class StackNode {
+    //     int data;
+    //     StackNode next;
+    //     StackNode(int a) {
+    //         data = a;
+    //         next = null;
+    //     }
+    // }
+    StackNode top;
+
+    // Function to push an integer into the stack.
+    void push(int a) {
+        StackNode newNode = new StackNode(a);
+        if(top == null){
+            top = newNode;
+            return;
+        }
+        newNode.next = top;
+        top = newNode;
+    }
+
+    // Function to remove an item from top of the stack.
+    int pop() {
+        if(top == null) return -1;
+        
+        int val = top.data;
+        top = top.next;
+        return val;
+        
+    }
+}
