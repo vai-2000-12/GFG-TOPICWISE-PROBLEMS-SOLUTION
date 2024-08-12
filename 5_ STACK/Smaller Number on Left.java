45bb67bebe2f5be96e6323efd57a3e3783ed67d5
vaibhav Soni <vaibhavsoni1712@gmail.com>
2024-08-12 13:48:02 +0530

    commit

---
+++ 5_ STACK/Smaller Number on Left.java
@@ -0,0 +1,31 @@
+class Solution{
+    static List<Integer> leftSmaller(int n, int a[])
+    {
+         Stack<Integer> stack = new Stack<>();
+        List<Integer> as = new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            if(stack.isEmpty())
+            {
+                as.add(-1);
+            }else if(stack.size()>0 && stack.peek()<a[i])
+            {
+                 as.add(stack.peek());
+            }else if(stack.size()>0 && stack.peek()>=a[i])
+            {
+                while(stack.size()>0 && stack.peek()>=a[i])
+                {
+                    stack.pop();
+                }
+                if(stack.size() == 0)
+                {
+                       as.add(-1);
+                }else{
+                       as.add(stack.peek());
+                }
+            }
+            stack.push(a[i]);
+        }
+        return as;
+    }
+}
