9b42ee45ebde8a4633da2ea4cd73d8863b0e0cd4
vaibhav Soni <vaibhavsoni1712@gmail.com>
2024-08-15 10:14:46 +0530

    commit

--- 6_MATRIX/Surround the 1's.java
+++ 6_MATRIX/Surround the 1's.java
@@ -5,13 +5,13 @@ class Solution
     {
         int m = matrix.length;
         int n = matrix[0].length;
-        int count = 0;
         int finalAns = 0;
         
         for( int i = 0 ; i < m ; i++){
-           for( int j = 0 ; j < n ; j++){
-               if(matrix[i][j] == 1){
-                 
+            for( int j = 0 ; j < n ; j++){
+                if(matrix[i][j] == 1){
+                int count = 0;
+                    
                  if(j + 1 < m && matrix[i][j+1] == 0)count++; //RD
                  if(j - 1 >= 0  && matrix[i][j-1] == 0)count++; //LD
                  if(i + 1 < n  && matrix[i+1][j] == 0)count++; //DoD
