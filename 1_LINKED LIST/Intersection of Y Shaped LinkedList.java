5cf629addaf03fa42b5f078db1fb7f38a0e2c962
Vaibhav soni <vaibhavsoni1712@gmail.com>
2024-10-05 22:49:07 +0530

    commit

---
+++ 1_LINKED LIST/Intersection of Y Shaped LinkedList.java
@@ -0,0 +1,24 @@
+class Intersect {
+    
+    int intersectPoint(Node head1, Node head2) {
+        Node tmp1  = head1;
+        Node tmp2  = head2;
+        
+        while (tmp1 != tmp2){
+            tmp1 = tmp1.next;
+            tmp2 = tmp2.next;
+             
+          if( tmp1 == tmp2){
+              return tmp1.data;
+          }     
+            if(tmp1 == null){
+                tmp1 = head2;
+            }
+            
+            if(tmp2 == null){
+                 tmp2 = head1;
+            }
+        }
+        return tmp1.data;
+    }
+}
