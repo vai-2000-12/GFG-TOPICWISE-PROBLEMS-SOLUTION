2dbe4da2c35e9780d7ae373d8f18612860dad4b8
Vaibhav soni <vaibhavsoni1712@gmail.com>
2024-11-30 17:33:47 +0530

    commīt

---
+++ 7_BACKTRACKING/Unique Subsets.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class solve {
+    // Function to find all possible unique subsets.
+    public static void solve(int[] arr, int n, int indx, ArrayList<Integer> curr, ArrayList<ArrayList<Integer>> res) {
+
+        res.add(new ArrayList<>(curr));
+
+        for (int i = indx; i < n; i++) {
+
+            if (i > indx && arr[i] == arr[i - 1])
+                continue;
+
+            curr.add(arr[i]);
+            // Recursive Call
+            solve(arr, n, i + 1, curr, res);
+            // Backtrack
+            curr.remove(curr.size() - 1);
+        }
+
+    }
+
+    public static ArrayList<ArrayList<Integer>> AllSubsets(int arr[], int n) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        ArrayList<Integer> curr = new ArrayList<>();
+        Arrays.sort(arr);
+        solve(arr, n, 0, curr, res);
+        return res;
+    }
+}
